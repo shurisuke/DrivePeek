81dad44dcf014b62d18c075d0786fc76354a7242
shurisuke <inoseshuri@icloud.com>
2025-11-30 17:58:26 +0900

    add: devise導入 Userモデル生成（#16）

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get "static_pages/top"
-  # 標準のヘルスチェック用エンドポイント
+  devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
-
   root to: "static_pages#top"
 end
