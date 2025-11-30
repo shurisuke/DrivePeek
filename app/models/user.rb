ce56c2b84e6a125a1f6f78760ab53a17aea07854
shurisuke <inoseshuri@icloud.com>
2025-11-30 20:10:49 +0900

    update: UserモデルStatusカラムのPublicをactiveに変更(#16)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # ステータス管理
-  enum status: { public: 0, private: 1 }
+  enum status: { active: 0, private: 1 }
 
   # バリデーション
   validates :name, presence: true, length: { maximum: 50 }
