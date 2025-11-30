9e2c4443c9377c8f2c85fa0bc9601f0856c2fd86
shurisuke <inoseshuri@icloud.com>
2025-11-30 18:25:28 +0900

    update: Userモデルにバリデーションとenum設定を追加(#16)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Deviseのモジュール（必要に応じて調整）
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # ステータス管理
+  enum status: { public: 0, private: 1 }
+
+  # バリデーション
+  validates :name, presence: true, length: { maximum: 50 }
+
+  validates :email, presence: true, uniqueness: true,
+                    format: { with: URI::MailTo::EMAIL_REGEXP }
 end
