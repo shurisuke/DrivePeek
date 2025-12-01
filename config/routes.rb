8e37cf15ff3db4088ca6de665ced83c3e2d1c167
shurisuke <inoseshuri@icloud.com>
2025-12-01 10:43:15 +0900

    update: ログイン状態でroot切替を実装(#19)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
+  get "dashboard/top"
   devise_for :users, controllers: {
     registrations: "users/registrations", # 新規登録画面
     sessions: "users/sessions", # ログイン画面
     passwords: "users/passwords" # パスワード再設定リクエスト画面
   }
 
-  root to: "static_pages#top"
+  # ログイン時のルート
+  authenticated :user do
+    root to: 'dashboard#top', as: :authenticated_root
+  end
+  # 非ログイン時のルート
+  unauthenticated do
+    root to: 'static_pages#top', as: :unauthenticated_root
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
