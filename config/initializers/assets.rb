ef3ba08ff8fdbe77962239830ed0ae641a7769cb
shurisuke <inoseshuri@icloud.com>
2025-12-01 19:32:46 +0900

    add: ヘッダー・フッターを作成(#21)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,3 +8,4 @@ Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/dist/js")
 Rails.application.config.assets.precompile += %w(bootstrap.min.js popper.js)
+Rails.application.config.assets.paths << Rails.root.join("node_modules/@fortawesome/fontawesome-free/webfonts")
