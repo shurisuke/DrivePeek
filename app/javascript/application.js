9a6d4032133d838dc1546f6611d34de5c2f3ed6b
shurisuke <inoseshuri@icloud.com>
2025-12-03 15:40:02 +0900

    setup:jQuery導入(#20)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,13 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "bootstrap"
 import "@popperjs/core"
+import $ from 'jquery'
+window.$ = $
+window.jQuery = $
+$(function() {
+  $("#Hamburger").click(function(){
+    $(this).toggleClass('js-menu-open');
+    $('.sp__menu').toggleClass('js-open');
+    $('#menu__hamburger__nav li a').toggleClass('js-menu-open');
+  });
+});
