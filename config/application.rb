ab4e1dddde4a28ce006f056f9924514bb1537f4f
shurisuke <inoseshuri@icloud.com>
2025-12-04 00:00:19 +0900

    fix: GitHub Actionsで発生したCSSビルドエラーの修正（#20）

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module DrivePeek
     config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.1
+    config.assets.css_build_file = "application.bootstrap.scss"
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
