26c8e6de07469302e400cba06f0f76050ac68721
shurisuke <inoseshuri@icloud.com>
2025-12-04 17:40:57 +0900

    fix: Userモデルstatusのenum名称を変更(#24)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # Enum（アカウントの公開状態）
-  enum :status, { public: 0, private: 1, deactivated: 2 }
+  enum :status, { active: 0, hidden: 1 }
 
   # Associations
   has_many :plans, dependent: :destroy
