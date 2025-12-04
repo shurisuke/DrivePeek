0147083cbb8de4176ebf612feaa884381e190dde
shurisuke <inoseshuri@icloud.com>
2025-12-05 04:58:38 +0900

    add :デプロイエラー修正(production.rb)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # ホスト制限対策
-  config.hosts << "your-app-name.onrender.com"
+  config.hosts << "drivepeek.onrender.com"
 
   # Settings specified here will take precedence over those in config/application.rb.
 
