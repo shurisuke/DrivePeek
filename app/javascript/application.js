fb1e851f9b67bcf61fa4f8e39b0e8ff0a62ae0fe
shurisuke <inoseshuri@icloud.com>
2025-12-05 03:47:39 +0900

    fix:package.json修正11

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,4 @@
-import Rails from "@rails/ujs"
-Rails.start()
-
-import jquery from "jquery"
-window.$ = window.jQuery = jquery
-window.bootstrap = require("bootstrap")
-
-$(function() {
-  $("#Hamburger").click(function(){
-    $(this).toggleClass('js-menu-open');
-    $('.sp__menu').toggleClass('js-open');
-    $('#menu__hamburger__nav li a').toggleClass('js-menu-open');
-  });
-});
+import "@hotwired/turbo-rails"
+import "controllers/application"
+import "bootstrap"
+import "hamburger"
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,9 +4,11 @@
 Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
-Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/dist/js")
-Rails.application.config.assets.precompile += %w[bootstrap.min.js popper.js]
 Rails.application.config.assets.paths << Rails.root.join("node_modules/@fortawesome/fontawesome-free/webfonts")
-Rails.application.config.assets.precompile += %w[ application.bootstrap.scss ]
+
+# CSSとJavaScriptのプリコンパイル設定
+Rails.application.config.assets.precompile += %w[
+  application.bootstrap.scss
+  application.js
+]
