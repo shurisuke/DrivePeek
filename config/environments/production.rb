641d0f60eb8948fb078c3e4d0c9afa8dacfd00ed
shurisuke <inoseshuri@icloud.com>
2025-12-04 19:26:17 +0900

    setup: デプロイに必要な設定を追記(#25)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  # ホスト制限対策
+  config.hosts << "your-app-name.onrender.com"
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
