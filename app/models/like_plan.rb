b6fa14c3df4bf7fae9f75eecd5d4dc96fe93e613
shurisuke <inoseshuri@icloud.com>
2025-12-04 17:38:40 +0900

    add: 各モデルにバリデーションとアソシエーションを追加(#24)

--- app/models/like_plan.rb
+++ app/models/like_plan.rb
@@ -1,4 +1,8 @@
 class LikePlan < ApplicationRecord
+  # Associations
   belongs_to :user
   belongs_to :plan
+
+  # Validations
+  validates :user_id, uniqueness: { scope: :plan_id }
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,8 @@
 class Tag < ApplicationRecord
+  # Associations
+  has_many :user_spot_tags, dependent: :destroy
+  has_many :user_spots, through: :user_spot_tags
+
+  # Validations
+  validates :tag_name, presence: true, uniqueness: true
 end
