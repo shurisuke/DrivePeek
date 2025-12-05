cddc37115f0dcaf8377444caf9b7481c6c7b6bed
shurisuke <inoseshuri@icloud.com>
2025-12-05 19:41:10 +0900

    add: planルーティングの追加(#26)

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   get "dashboard/top"
+
+  # Devise関連
   devise_for :users, controllers: {
     registrations: "users/registrations", # 新規登録画面
     sessions: "users/sessions", # ログイン画面
     passwords: "users/passwords" # パスワード再設定リクエスト画面
   }
-
-  devise_scope :user do
-    get "/users/sign_out" => "devise/sessions#destroy"
-  end
 
   # ログイン時のルート
   authenticated :user do
@@ -19,13 +17,19 @@ Rails.application.routes.draw do
     root to: "static_pages#top", as: :unauthenticated_root
   end
 
-  # 非ログイン時のルート
+  # ログアウト時のアクション
+  devise_scope :user do
+    get "/users/sign_out" => "devise/sessions#destroy"
+  end
+
+  # マイページ
   namespace :account do
-    get "profiles/show"
-    get "profiles/edit"
-    get "profiles/update"
     resource :profile, only: [ :show, :edit, :update ]
   end
 
+  # プラン
+  resources :plans
+
+  # ヘルスチェック
   get "up" => "rails/health#show", as: :rails_health_check
 end
