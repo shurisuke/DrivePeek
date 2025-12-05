0dac1d44debce0ba47431f6bef8361b30eb0aefd
shurisuke <inoseshuri@icloud.com>
2025-12-06 01:03:09 +0900

    setup: cssファイル読み込みに不必要な設定と記述を削除(#26)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,9 +6,3 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
 Rails.application.config.assets.paths << Rails.root.join("node_modules/@fortawesome/fontawesome-free/webfonts")
-
-# CSSとJavaScriptのプリコンパイル設定
-Rails.application.config.assets.precompile += %w[
-  application.bootstrap.scss
-  application.js
-]
