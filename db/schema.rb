8ef5eec381ebdd5d0476a832483994d835921086
shurisuke <inoseshuri@icloud.com>
2025-12-11 02:35:18 +0900

    change: plans.titleカラムのnull: false制約を削除(#125)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_10_094450) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_10_173307) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -70,7 +70,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_10_094450) do
 
   create_table "plans", force: :cascade do |t|
     t.datetime "created_at", null: false
-    t.string "title", null: false
+    t.string "title"
     t.integer "total_cost", default: 0, null: false
     t.float "total_distance", default: 0.0, null: false
     t.integer "total_time", default: 0, null: false
