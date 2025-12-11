dade4a337a8795b6c7ca7c4192c2b51e97d7f92c
shurisuke <inoseshuri@icloud.com>
2025-12-12 05:41:53 +0900

    feature: plan#create&editアクションにに@plan代入処理を追加(#125)

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -12,10 +12,11 @@ class PlansController < ApplicationController
     @plan = PlanSetupService.new(user: current_user, lat: lat, lng: lng).setup
     redirect_to edit_plan_path(@plan)
   rescue => e
-    redirect_to authenticated_root_path, alert: "プランの作成に失敗しました: #{e.message}"
+    redirect_to authenticated_root_path(@plan), alert: "プランの作成に失敗しました: #{e.message}"
   end
 
   def edit
+    @plan = Plan.includes(:start_point, :goal_point, :plan_spots => :spot).find(params[:id])
   end
 
   def update
