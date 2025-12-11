53c666eb2a79794b7ab8ddffbc5fedf25fa9efb2
shurisuke <inoseshuri@icloud.com>
2025-12-12 02:35:18 +0900

    feature: 出発地点をもとに帰宅地点を登録するメソッドを作成(#125)

--- app/models/goal_point.rb
+++ app/models/goal_point.rb
@@ -1,7 +1,11 @@
 class GoalPoint < ApplicationRecord
-  # Associations
   belongs_to :plan
 
-  # Validations
   validates :lat, :lng, :address, presence: true
+
+  def self.build_from_start_point(plan:, start_point:)
+    plan.build_goal_point(
+      start_point.attributes.slice("lat", "lng", "address")
+    )
+  end
 end
