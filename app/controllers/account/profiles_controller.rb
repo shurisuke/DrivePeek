2b3ca81c97ec96f218c5f7695f9d3ab1ad94ec6e
shurisuke <inoseshuri@icloud.com>
2025-12-26 03:36:45 +0900

    feat: アカウント管理画面で公開設定を非同期更新 (#183)
    - profiles_controllerにJSON対応のupdateアクション追加
    - profile_status_controller.jsで非同期更新
    - ユーザー名・メールアドレスを実データ表示に変更

--- app/controllers/account/profiles_controller.rb
+++ app/controllers/account/profiles_controller.rb
@@ -6,5 +6,22 @@ class Account::ProfilesController < ApplicationController
   end
 
   def update
+    if current_user.update(profile_params)
+      respond_to do |format|
+        format.html { redirect_to account_profile_path, notice: "設定を更新しました" }
+        format.json { render json: { success: true, message: "設定を更新しました" } }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to account_profile_path, alert: "更新に失敗しました" }
+        format.json { render json: { success: false, errors: current_user.errors.full_messages }, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:user).permit(:status)
   end
 end
