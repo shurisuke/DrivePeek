e1279da1b5326ce5d28ea6bd2d223ddd17dd282c
shurisuke <inoseshuri@icloud.com>
2026-01-11 23:40:55 +0900

    feat: コントローラにお気に入りフィルター対応を追加 (#289)
    - PlansController: set_filter_variablesに@favorites_onlyを追加
    - NavibarsController: showアクションでお気に入りフィルターをサポート

--- app/controllers/navibars_controller.rb
+++ app/controllers/navibars_controller.rb
@@ -3,20 +3,25 @@ class NavibarsController < ApplicationController
   before_action :set_plan
 
   def show
+    @search_query = params[:q]
+    @selected_cities = Array(params[:cities]).reject(&:blank?)
+    @selected_genre_ids = Array(params[:genre_ids]).map(&:to_i).reject(&:zero?)
+    @favorites_only = params[:favorites_only] == "1"
+    @genres = Genre.ordered
+    @cities_by_prefecture = Spot.cities_by_prefecture
+
+    # お気に入りフィルターが有効な場合、current_user を渡す
+    liked_by_user = @favorites_only ? current_user : nil
+
     # みんなのプラン: 編集中のプランを除外
     @community_plans = Plan.for_community(
       keyword: params[:q],
       cities: params[:cities],
-      genre_ids: params[:genre_ids]
+      genre_ids: params[:genre_ids],
+      liked_by_user: liked_by_user
     ).where.not(id: @plan.id)
       .page(params[:page])
       .per(6)
-
-    @search_query = params[:q]
-    @selected_cities = Array(params[:cities]).reject(&:blank?)
-    @selected_genre_ids = Array(params[:genre_ids]).map(&:to_i).reject(&:zero?)
-    @genres = Genre.ordered
-    @cities_by_prefecture = Spot.cities_by_prefecture
   end
 
   private
