6fbede61b26a3e61482789b65182a7eb33c40c3c
shurisuke <inoseshuri@icloud.com>
2026-01-26 21:51:38 +0900

    feat: ログイン状態の自動保持機能を追加 (#365)
    - ログイン時に自動でremember_meを有効化
    - 保持期間を2週間に設定
    - アクセス時に保持期間を延長する設定を有効化

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,7 @@
 class Users::SessionsController < Devise::SessionsController
+  # ログイン時に自動でremember_meを有効化
+  before_action :configure_sign_in_params, only: [:create]
+
   # ログイン後のリダイレクト先をプラン作成エントリー画面に変更
   def after_sign_in_path_for(resource)
     new_plan_path
@@ -7,5 +10,13 @@ class Users::SessionsController < Devise::SessionsController
   # ログアウト後のリダイレクト先をログイン前トップページへ変更
   def after_sign_out_path_for(resource_or_scope)
     unauthenticated_root_path
+  end
+
+  private
+
+  # 常にremember_meを有効にする
+  def configure_sign_in_params
+    params[:user] ||= {}
+    params[:user][:remember_me] = "1"
   end
 end
