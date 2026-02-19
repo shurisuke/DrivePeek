0a3f352de2f7c834a1c80c75db1d414e3eddf7fe
shurisuke <inoseshuri@icloud.com>
2026-02-19 22:06:51 +0900

    feat: スポット詳細で関連スポット取得 (#539)

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -2,5 +2,7 @@ class SpotsController < ApplicationController
   def show
     @spot = Spot.includes(:genres).find(params[:id])
     @favorite_spot = current_user&.favorite_spots&.find_by(spot: @spot)
+    @related_spots = @spot.related_spots(limit: 5)
+    @related_preload = Spot.preload_card_data(@related_spots, current_user)
   end
 end
